0e84e59699849b10e82b699c1d274871af6ea00c
robots <thomvray@gmail.com>
2025-07-31 10:15:56 +0100

    f

---
+++ Swarm/Project_sec_session/SR_Project_H414/build/CMakeFiles/shepherding.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shepherding.dir/shepherding.cpp.o"
+  "CMakeFiles/shepherding.dir/shepherding.cpp.o.d"
+  "libshepherding.pdb"
+  "libshepherding.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shepherding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Swarm/Project_sec_session/SR_Project_H414/build/CMakeFiles/shepherding.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shepherding.
+# This may be replaced when dependencies are built.
